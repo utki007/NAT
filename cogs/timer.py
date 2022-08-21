a094b47e00b797390f0f3592e0529b85c19e7db3
Utkarsh Narain <utkarshnarain007@gmail.com>
2022-08-21 23:26:52 +0530

    Timer Re-ping added

--- cogs/timer.py
+++ cogs/timer.py
@@ -15,23 +15,22 @@ def chunk(it, size):
 class Button(discord.ui.View):
 	def __init__(self,timer_data):
 		super().__init__(timeout=None)
-		self.message = None #req for disabling buttons after timeout
+		self.message = None
 		self.timer = timer_data
 	
 	@discord.ui.button(style=discord.ButtonStyle.gray , emoji="<a:tgk_timer:841624339169935390>",custom_id='timer_button')
 	async def click_me(self, interaction: discord.Interaction, button: discord.ui.Button):
-		# await interaction.response.defer()
 		timer_data = await interaction.client.timer.find({'_id':interaction.message.id})
 		if timer_data == None:
 			return await interaction.response.send_message("Invalid/Expired timer", ephemeral=True)
 
 		if interaction.user.id in timer_data['members']:
-			return await interaction.response.send_message("You have already entered the timer!", ephemeral=True)
+			return await interaction.response.send_message("<a:nat_cross:1010969491347357717> **|** You have already entered the timer!", ephemeral=True)
 		timer_data['members'].append(interaction.user.id)
 
 		button.label = len(timer_data['members'])
 		await interaction.message.edit(view=self)
-		await interaction.response.send_message("You will be reminded once timer ends!", ephemeral=True)
+		await interaction.response.send_message("<a:nat_check:1010969401379536958> **|** I will remind you once timer ends!", ephemeral=True)
 		await interaction.client.timer.update(timer_data)
 		self.timer = timer_data
 	
@@ -49,7 +48,8 @@ class timer_slash(app_commands.Group):
 
 	@app_commands.command(name="start", description="Create a timer")
 	@app_commands.guild_only()
-	# @app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id, i.user.id))
+	@app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id, i.user.id))
+	@app_commands.describe(time = "Enter time in format: 1h30m2s")
 	async def timerStart(self, interaction:  discord.Interaction, time: str):
 		try:
 			time = await convert_to_time(time)
@@ -83,7 +83,6 @@ class timer_slash(app_commands.Group):
 		desc = f"\n"
 
 		e = discord.Embed(
-			# color= 0x5865F2,
 			color=0xE74C3C,
 			title=f"{'Timer'}",
 			description = f"> **Ends:** **<t:{int(datetime.datetime.timestamp(end))}:R>**\n> **Launched by:** <@{timer_data['user_id']}>\n",
@@ -101,7 +100,8 @@ class timer_slash(app_commands.Group):
 
 	@app_commands.command(name="end", description="End a timer")
 	@app_commands.guild_only()
-	# @app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id, i.user.id))
+	@app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id, i.user.id))
+	@app_commands.describe(message_id = "Enter Message ID of an active timer")
 	async def timerEnd(self, interaction:  discord.Interaction, message_id: str):
 		await interaction.response.defer(ephemeral=True)
 		try:
@@ -120,7 +120,8 @@ class timer_slash(app_commands.Group):
 
 	@app_commands.command(name="delete", description="Delete a timer")
 	@app_commands.guild_only()
-	# @app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id, i.user.id))
+	@app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id, i.user.id))
+	@app_commands.describe(message_id = "Enter Message ID of an active timer")
 	async def timerDelete(self, interaction:  discord.Interaction, message_id: str):
 		await interaction.response.defer(ephemeral=True)
 		try:
@@ -142,6 +143,58 @@ class timer_slash(app_commands.Group):
 			await message.delete()
 			await interaction.client.timer.delete(timer_data)
 			await interaction.edit_original_response(content="Timer deleted successfully!")
+		except:
+			warning = discord.Embed(
+				color=0xFF0000,
+				title=f"<a:nat_warning:1010618708688912466> **|** Invalid message id")
+			return await interaction.edit_original_response(embed=warning)
+
+	@app_commands.command(name="re-ping", description="Ping from an expired timer!")
+	@app_commands.guild_only()
+	@app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id, i.user.id))
+	@app_commands.describe(message_id = "Inactive timer message id < 1h")
+	async def timerPing(self, interaction:  discord.Interaction, message_id: str):
+		await interaction.response.defer(ephemeral=True)
+		try:
+			timer_data = await interaction.client.timer.find({'_id': int(message_id)})
+			if timer_data == None:
+				return await interaction.edit_original_response(content="Cannot ping from a timer that has ended more than an hour ago!")
+			if timer_data['user_id'] != interaction.user.id:
+				return await interaction.edit_original_response(content="You are not the host of the timer!")
+			if timer_data['time'] > datetime.datetime.now():
+				return await interaction.edit_original_response(content="Timer has not ended yet!")
+			if len(timer_data['members']) == 0:
+				return await interaction.edit_original_response(content="No one has entered the timer!")
+			
+			channel = self.bot.get_channel(timer_data['channel_id'])
+			if channel == None:
+				self.bot.remove_view(Button(timer_data))
+				return await self.bot.timer.delete(timer_data['_id'])
+			try:
+				message = await channel.fetch_message(timer_data['_id'])
+			except discord.NotFound:
+				self.bot.remove_view(Button(timer_data))
+				return await self.bot.timer.delete(timer_data['_id'])
+			
+			member_list = [await self.bot.fetch_user(member) for member in timer_data['members']]
+			member_list = [member for member in member_list if member != None]
+
+			try:
+				ping_group = list(chunk(member_list,30))
+				for members in ping_group:
+					await channel.send(f"{', '.join(user.mention for user in members)}",delete_after=1)
+			except:
+				pass
+
+			try:
+				view = discord.ui.View()
+				view.add_item(discord.ui.Button(label=f'Timer link', url=message.jump_url))
+				end_message = await message.reply(f"<@{timer_data['user_id']}> your timer has been repinged!", view=view)
+				await end_message.add_reaction("<a:gk_waiting:945772518776664104>")
+				await self.bot.timer.delete(timer_data['_id'])
+			except:
+				pass
+			await interaction.edit_original_response(content="Ping sent successfully!")
 		except:
 			warning = discord.Embed(
 				color=0xFF0000,
@@ -183,8 +236,14 @@ class timer(commands.Cog):
 			return await self.bot.timer.delete(timer_data['_id'])
 
 		embed = message.embeds[0]
-		embed.description = f"> **Entrants:** {len(timer_data['members'])}\n> **Timer ended!**\n> **Launched by:** <@{timer_data['user_id']}>"
+		if "Timer ended!" in embed.description:
+			time_diff_to_delete = int((datetime.datetime.now() - timer_data['time']).total_seconds())
+			if time_diff_to_delete > 60*60:
+				return await self.bot.timer.delete(timer_data['_id'])
+			return
+		embed.description = f"> **Entrants:** **{len(timer_data['members'])}**\n> **Timer ended!**\n> **Launched by:** <@{timer_data['user_id']}>"
 		embed.timestamp = datetime.datetime.now()
+		timer_data['time'] = datetime.datetime.now()
 
 		view = discord.ui.View.from_message(message)
 		for children in view.children:
@@ -193,11 +252,12 @@ class timer(commands.Cog):
 		await message.edit(embed=embed,view=view)
 
 		member_list = [await self.bot.fetch_user(member) for member in timer_data['members']]
+		member_list = [member for member in member_list if member != None]
 
 		try:
 			ping_group = list(chunk(member_list,30))
-			for memmbers in ping_group:
-				await channel.send(f"{', '.join(user.mention for user in memmbers)}",delete_after=1)
+			for members in ping_group:
+				await channel.send(f"{', '.join(user.mention for user in members)}",delete_after=1)
 		except:
 			pass
 
@@ -208,8 +268,8 @@ class timer(commands.Cog):
 			await end_message.add_reaction("<a:gk_waiting:945772518776664104>")
 		except:
 			pass
-
-		await self.bot.timer.delete(timer_data['_id'])
+		
+		await self.bot.timer.update(timer_data)
 
 	@tasks.loop(seconds=60)
 	async def timer_loop(self):
@@ -227,58 +287,6 @@ class timer(commands.Cog):
 	async def before_timer_loop(self):
 		await self.bot.wait_until_ready()
 
-	# @app_commands.command(name="timer", description="Set a timer")
-	# @app_commands.guild_only()
-	# # @app_commands.checks.cooldown(1, 30, key=lambda i: (i.guild_id, i.user.id))
-	# async def timer(self, interaction:  discord.Interaction, time: str):
-	# 	try:
-	# 		time = await convert_to_time(time)
-	# 		cd = int(await calculate(time))
-	# 		end = datetime.datetime.now() + datetime.timedelta(seconds=cd)
-	# 	except:
-	# 		warning = discord.Embed(
-	# 			color=0xFF0000,
-	# 			title=f"<a:nat_warning:1010618708688912466> **|** Incorrect time format, please use `1h30m10s`")
-	# 		return await interaction.response.send_message(embed=warning, ephemeral=True)
-
-	# 	if cd < 10:
-	# 		warning = discord.Embed(
-	# 			color=0xFF0000,
-	# 			title=f"<a:nat_warning:1010618708688912466> **|** Time should be more than 10s")
-	# 		return await interaction.response.send_message(embed=warning, ephemeral=True)
-	# 	await interaction.response.defer()
-	# 	timer_left = datetime.datetime.strptime(str(datetime.timedelta(seconds=cd)), '%H:%M:%S')
-	# 	message = await interaction.original_response()
-	# 	timer_data = {
-	# 		"_id": message.id,
-	# 		'user_id': interaction.user.id,
-	# 		'guild_id': interaction.guild.id,
-	# 		'channel_id': interaction.channel.id,
-	# 		'host_id': interaction.user.id,
-	# 		'time': end,
-	# 		'members': []
-	# 	}
-	# 	await interaction.client.timer.upsert(timer_data)
-
-	# 	desc = f"\n"
-
-	# 	e = discord.Embed(
-	# 		# color= 0x5865F2,
-	# 		color=0xE74C3C,
-	# 		title=f"{'Timer'}",
-	# 		description = f"> **Ends:** **<t:{int(datetime.datetime.timestamp(end))}:R>**\n> **Launched by:** <@{timer_data['user_id']}>\n",
-	# 		timestamp=end
-	# 	)
-	# 	e.set_footer(
-	# 			text=f"Ends at")
-
-	# 	view = Button(timer_data)
-	# 	timer = await interaction.edit_original_response(embed=e, view=view)
-	# 	view.message = timer
-
-	# 	if cd < 90:
-	# 		self.bot.dispatch('timer_end', timer_data, True)
-
-
+	
 async def setup(bot):
 	await bot.add_cog(timer(bot))
