a37a2cdcf53cd8b69e24b1538fff5d94ebcc74a4
Utkarsh Narain <utkarshnarain007@gmail.com>
2022-08-23 19:33:36 +0530

    Added time convertor

--- utils/convertor.py
+++ utils/convertor.py
@@ -1,6 +1,7 @@
 import time
 import discord
 import math
+import datetime
 from TagScriptEngine import Interpreter, adapter, block
 from discord.ext import commands,tasks
 
@@ -45,3 +46,38 @@ async def millify(n):
 
     # return '{:.1f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
     return f'{round(n / 10**(3 * millidx),1):,}{millnames[millidx]}'
+
+async def convert_to_human_time(seconds):
+    
+    days, remainder = divmod(seconds, 86400)
+    hours, remainder = divmod(remainder , 3600)
+    minutes, seconds = divmod(remainder, 60)
+        
+    human_timer = []
+    if int(days)>0:
+        if int(days)==1:
+            human_timer.append(f"{int(days)} day")
+        else:
+            human_timer.append(f"{int(days)} days")
+    if int(hours)>0:
+        if int(hours) == 1:
+            human_timer.append(f'{int(hours)} hour')
+        else:
+            human_timer.append(f'{int(hours)} hours')
+    if int(minutes)>0:
+        if int(minutes) == 1:
+            human_timer.append(f'{int(minutes)} minute')
+        else:
+            human_timer.append(f'{int(minutes)} minutes')
+    if int(seconds)>0:
+        if int(seconds) == 1:
+            human_timer.append(f'{int(seconds)} second')
+        else:
+            human_timer.append(f'{int(seconds)} seconds')
+    
+    if len(human_timer)>1:
+        timer = f'{", ".join(i for i in human_timer[:-1])} and {human_timer[-1]}'
+    else:
+        timer = human_timer[-1]
+
+    return timer.strip()
