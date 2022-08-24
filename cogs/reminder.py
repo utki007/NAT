eaee6323ed185b3ca3b5cb41bfb8a495ffed619e
Utkarsh Narain <utkarshnarain007@gmail.com>
2022-08-24 12:50:26 +0530

    Added reminder subscirbe

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -7,7 +7,8 @@ import discord
 from discord import app_commands
 from discord.ext import commands, tasks
 from utils.convertor import *
-from utils.paginator import Paginator
+from utils.views.paginator import Paginator
+from utils.views.confirm import Confirm
 
 
 class reminder_slash(app_commands.Group):
@@ -31,7 +32,7 @@ class reminder_slash(app_commands.Group):
 				title=f"<a:nat_warning:1010618708688912466> **|** Incorrect time format, please use `1h30m10s`")
 			return await interaction.response.send_message(embed=warning, ephemeral=True)
 
-		if cd < 3:
+		if cd < 30:
 			warning = discord.Embed(
 				color=self.bot.color['danger'],
 				title=f"<a:nat_warning:1010618708688912466> **|** Time should be more than 30s")
@@ -57,7 +58,7 @@ class reminder_slash(app_commands.Group):
 			'message_id': message.id,
 			'message_link': message.jump_url,
 			'about': about,
-			'time': end,
+			'time': datetime.datetime.now() + datetime.timedelta(seconds=cd),
 			'created_at': datetime.datetime.now(),
 			'remind_in_seconds': cd,
 			'members': [interaction.user.id]
@@ -65,7 +66,7 @@ class reminder_slash(app_commands.Group):
 		await interaction.client.doc_remider.upsert(reminder_data)
 
 		await interaction.edit_original_response(
-			content=f"Alright **{interaction.user.name}**, I'll remind you about `{about}` in **{await convert_to_human_time(cd)}**. (at <t:{int(datetime.datetime.timestamp(end))}>)"
+			content=f"Alright **{interaction.user.name}**, I'll remind you about `{about}` in **{await convert_to_human_time(cd)}**. (at <t:{int(datetime.datetime.timestamp(datetime.datetime.now() + datetime.timedelta(seconds=cd)))}>)"
 			f"\nThis reminder's ID is `{reminder_data['_id']}`."
 		)
 		if cd < 90:
@@ -94,22 +95,52 @@ class reminder_slash(app_commands.Group):
 			await interaction.client.doc_remider.delete(reminder_data)
 			return await interaction.edit_original_response(content=f"You have successfully deleted **Reminder #{reminder_data['_id']}** for `{reminder_data['about']}`!")
 
-	@app_commands.command(name="clear", description="Clear all reminders")
+	@app_commands.command(name="clear", description="Clears all reminders")
 	@app_commands.guild_only()
 	@app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id, i.user.id))
 	async def reminderClear(self, interaction: discord.Interaction, clear_from: Literal['current server', 'all servers']):
-		await interaction.response.defer(ephemeral=True)
+		await interaction.response.defer(ephemeral=False)
 		if clear_from == 'current server':
 			reminders = await interaction.client.doc_remider.get_all({'host_id':interaction.user.id, 'guild_id': interaction.guild.id})
-			content = "You have successfully cleared all reminders from **{interaction.guild.name}**!"
+			content = f"You have successfully cleared all reminders from **{interaction.guild.name}**."
+			question = f"Do you wish to clear {len(reminders)} active reminders from **{interaction.guild.name}**?"
 		else:
 			reminders = await interaction.client.doc_remider.get_all({'host_id':interaction.user.id})
-			content = "You have successfully cleared all reminders from all your servers!"
+			content = "You have successfully cleared all reminders from all your servers."
+			question = f"Do you wish to clear {len(reminders)} active reminders from all your servers?"
+		
 		if reminders == None or reminders == []:
-			return await interaction.edit_original_response(content=f"All reminders are already cleared!")
-		for reminder in reminders:
-			await interaction.client.doc_remider.delete(reminder)
-		return await interaction.edit_original_response(content=content)
+			warning = discord.Embed(
+				color=self.bot.color['danger'],
+				title=f"<a:nat_cross:1010969491347357717> **|** Nothing to clear",
+				description=f"You don't have any reminders to clear.")
+			return await interaction.edit_original_response(embed=warning, content=None, view=None)
+
+		# sending a confirmation dialog box
+		view = Confirm()
+		await interaction.followup.send(content=question, view=view)
+		await view.wait()
+
+		if view.value is None:
+			warning = discord.Embed(
+				color=self.bot.color['danger'],
+				title=f"<a:nat_warning:1010618708688912466> **|** Timed out",
+				description=f"I couldn't clear your reminders because you were timed out.")
+			return await interaction.edit_original_response(embed=warning, content=None, view=None)
+		elif view.value:
+			for reminder in reminders:
+				await interaction.client.doc_remider.delete(reminder)
+			success_embed = discord.Embed(
+					color=self.bot.color['default'],
+					title=f"<a:nat_check:1010969401379536958> **|** Cleared up",
+					description=content)
+			return await interaction.edit_original_response(embed=success_embed, content=None, view=None)
+		else:
+			warning = discord.Embed(
+				color=self.bot.color['danger'],
+				title=f"<a:nat_cross:1010969491347357717> **|** Cancelled",
+				description=f'Come back once you are sure about what you are doing.')
+			return await interaction.edit_original_response(embed=warning, content=None, view=None)
 
 	@app_commands.command(name="list", description="List of all reminders")
 	@app_commands.guild_only()
@@ -155,8 +186,39 @@ class reminder_slash(app_commands.Group):
 			discord.ui.Button(label="Next", style=discord.ButtonStyle.gray),
 			# discord.ui.Button(label=">>", style=discord.ButtonStyle.gray)
 		]
-		await Paginator(interaction, pages, custom_button).start(embeded=True, quick_navigation=False) #set quick_navitation to Flase if len(pages) > 24 or you want to remove dromdown
+		await Paginator(interaction, pages, custom_button).start(embeded=True, quick_navigation=False)
 	
+	@app_commands.command(name="subscibe", description="Creates a copy of a reminder")
+	@app_commands.guild_only()
+	@app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id, i.user.id))
+	async def reminderSubscibe(self, interaction: discord.Interaction, reminder_id: int):
+		await interaction.response.defer(ephemeral=True)
+		reminder_data = await interaction.client.doc_remider.find({'_id': reminder_id})
+		if reminder_data == None:
+			return await interaction.edit_original_response(content="Invalid/Expired reminder!")
+		if reminder_data['host_id'] != interaction.user.id:
+			counter = await self.bot.doc_remider.find("reminder")
+			if counter == None:
+				id = 1997
+				await self.bot.doc_remider.insert({"_id": "reminder", "counter": id})
+			else:
+				counter["counter"] += 1
+			id = counter["counter"]
+			await self.bot.doc_remider.update(counter)
+			message = await interaction.original_response()
+			reminder_data['id'] = id
+			reminder_data['host_id'] = interaction.user.id
+			reminder_data['guild_id'] = interaction.guild.id
+			reminder_data['channel_id'] = interaction.channel.id
+			reminder_data['message_id'] = message.id
+			reminder_data['message_link'] = message.jump_url
+			return await interaction.edit_original_response(
+				content=f"Alright **{interaction.user.name}**, I'll remind you about `{reminder_data['about']}` in **{await convert_to_human_time((reminder_data['time'] - datetime.datetime.now()).total_seconds())}**. (at <t:{int(reminder_data['time'])}>)"
+				f"\nThis reminder's ID is `{reminder_data['_id']}`."
+			)
+		else:
+			return await interaction.edit_original_response(content="You are already subscribed to that reminder!")
+
 
 class reminder(commands.Cog):
 
@@ -191,16 +253,17 @@ class reminder(commands.Cog):
 		member_list = [member for member in member_list if member != None]
 
 		content = f'Your reminder ended: **{reminder_data["about"]}**'
+		time_passed = await convert_to_human_time((datetime.datetime.now() - reminder_data['created_at']).total_seconds())
 
 		reminder_embed = discord.Embed(
 			title=f'Reminder #{reminder_data["_id"]}',
-			description=f"You asked to be reminded for `{reminder_data['about']}` [{await convert_to_human_time(reminder_data['remind_in_seconds'])} ago]({message.jump_url}).",
+			description=f"You asked to be reminded for `{reminder_data['about']}` [{time_passed} ago]({message.jump_url}).",
 			color=self.bot.color['default']
 		)
 
 		for member in member_list:
 			try:
-				await member.send(content = content, embed = reminder_embed)
+				await member.send(content = content, embed = reminder_embed, allowed_mentions=discord.AllowedMentions(everyone = False, users = False, roles = False, replied_user = True))
 				await asyncio.sleep(0.5)
 			except:
 				await channel.send(content = f"{member.mention}, {content}", embed = reminder_embed)
