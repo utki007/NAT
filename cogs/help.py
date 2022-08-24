4a87bdeecb0a868acf40546e06dcaa91e1244182
Jay Patel <pateljay24004@gmail.com>
2022-08-24 22:20:05 +0530

    embed updated

--- cogs/help.py
+++ cogs/help.py
@@ -20,7 +20,8 @@ class Help(commands.Cog):
     @app_commands.autocomplete(command=command_auto_complete)
     async def _help(self, interaction: discord.Interaction, command: str):
         command = interaction.client.tree.get_command(command)
-        embed = discord.Embed(title=f"Info of {command.name}", description=f"{command.description}", color=interaction.client.color['default'])
+
+        embed = discord.Embed(title=f"Help | {command.name.title()}", description=f"{command.description}", color=interaction.client.color['default'])
         useage = ""
         query = ""
         useage += f"/{command.name}"
@@ -34,7 +35,7 @@ class Help(commands.Cog):
                     else:
                         extra_flags = True
 
-                    query += f"**{argument.display_name}:** `{argument.description}`\n<:nat_newline:1011695174281351179> Autocomplete? `{argument.autocomplete}`\n"
+                    query += f"**{argument.display_name}:** `{argument.description}`\n"
                 if extra_flags == True:
                     useage += f" <Extra Flags>"
                 embed.add_field(name="Usage:", value=f"`{useage}`")
@@ -48,14 +49,14 @@ class Help(commands.Cog):
                     pass
             await interaction.response.send_message(embed=embed)
         elif type(command) == app_commands.commands.Group:
-            embed = discord.Embed(title=f"Info of {command.name}", description=f"{command.description}", color=interaction.client.color['default'])
+            embed = discord.Embed(title=f"Help | {command.name}", description=f"{command.description}", color=interaction.client.color['default'])
             embed.description += f"\n\n**Available Commands:**\n"
             for subcommand in command.commands:
                 embed.description += f"`/{command.name} {subcommand.name}`\n"
             embed.set_footer(text=f"Use buttons to know more about each command")
             pages = [embed]
             for sub_command in command.commands:
-                sub_cmd_embed = discord.Embed(title=f"Info of {sub_command.name}", description=f"{sub_command.description}", color=interaction.client.color['default'])
+                sub_cmd_embed = discord.Embed(title=f"{command.name.title()} {sub_command.name}", description=f"{sub_command.description}", color=interaction.client.color['default'])
                 useage = ""
                 query = ""
                 useage += f"/{command.name} {sub_command.name}"
@@ -68,7 +69,7 @@ class Help(commands.Cog):
                         else:
                             extra_flags = True
 
-                        query += f"**{argument.display_name}:** `{argument.description}`\n<:nat_newline:1011695174281351179> Autocomplete? `{argument.autocomplete}`\n"
+                        query += f"**{argument.display_name}:** `{argument.description}`"
                     if extra_flags == True:
                         useage += f" <Extra Flags>"
                     sub_cmd_embed.add_field(name="Usage:", value=f"`{useage}`")
