b44d64ba8e5f05cbb6e38efbd3e1fc22ed07b31a
Jay Patel <pateljay24004@gmail.com>
2022-09-01 22:46:41 +0530

    added dump for role and channel

--- cogs/serverUtils.py
+++ cogs/serverUtils.py
@@ -1,18 +1,55 @@
 import time as t
 import datetime
 import discord
-from discord import app_commands
+from discord import app_commands, Interaction
 from discord.ext import commands
 from utils.convertor import *
+from io import BytesIO
+
+class Dump(commands.GroupCog, name="dump"):
+	def __init__(self, bot):
+		self.bot = bot
+	
+	@app_commands.command(name="role", description="Dump a role")
+	@app_commands.describe(role='Role to dump')
+	async def dump_role(self, interaction: Interaction, role:discord.Role):
+		msg = f"{role.name} has {len(role.members)} members"
+		
+		if len(role.members) > 20:
+			await interaction.response.send_message("Role has more than 20 members, sending as file", ephemeral=False)
+			members = [f"{member.name}#{member.discriminator} | {member.id}" for member in role.members]
+
+			buffer = BytesIO("\n".join(members).encode('utf-8'))
+			file = discord.File(buffer, filename=f"{role.name}.txt")
+			buffer.close()
+			await interaction.edit_original_response(content=msg, attachments=[file])
+		else:
+			embed = discord.Embed(title=f"{role.name} dump", description="\n".join([f"{member.name}#{member.discriminator} | {member.id}" for member in role.members]), color=role.color)
+			await interaction.response.send_message(content=msg, embeds=[embed])
+	
+	@app_commands.command(name="channel", description="Dump a channel")
+	@app_commands.describe(channel='Channel to dump')
+	async def dump_channel(self, interaction: Interaction, channel:discord.TextChannel):
+		msg = f"{channel.name} has {len(channel.members)} members"
+
+		if len(channel.members) > 20:
+			await interaction.response.send_message("Channel has more than 20 members, sending as file", ephemeral=False)
+			members = [f"{member.name}#{member.discriminator} | {member.id}" for member in channel.members]
+
+			buffer = BytesIO("\n".join(members).encode('utf-8'))
+			file = discord.File(buffer, filename=f"{channel.name}.txt")
+			buffer.close()
+			await interaction.edit_original_response(content=msg, attachments=[file])
+		
+		else:
+			embed = discord.Embed(title=f"{channel.name} dump", description="\n".join([f"{member.name}#{member.discriminator} | {member.id}" for member in channel.members]), color=self.bot.color['default'])
+			await interaction.response.send_message(content=msg, embeds=[embed])
+
 
 class serverUtils(commands.Cog):
-
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.Cog.listener()
-	async def on_ready(self):
-		print(f"{self.__class__.__name__} Cog has been loaded\n-----")
 
 	@app_commands.command(name="ping", description="Ping pong! 🏓")
 	@app_commands.checks.cooldown(1, 30, key=lambda i: (i.guild_id, i.user.id))
@@ -50,3 +87,5 @@ class serverUtils(commands.Cog):
 
 async def setup(bot):
 	await bot.add_cog(serverUtils(bot))
+	await bot.add_cog(Dump(bot))
+	print(f"loaded serverUtils cog")
