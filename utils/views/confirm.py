cfa5b383247c35709ed919d89f06754de4ddb054
Jay Patel <pateljay24004@gmail.com>
2022-09-11 13:36:30 +0530

    updated confirm view

--- utils/views/confirm.py
+++ utils/views/confirm.py
@@ -3,13 +3,18 @@ from discord.ext import commands
 from typing import Union
 # Define a simple View that gives us a confirmation menu
 class Confirm(discord.ui.View):
-	def __init__(self, user: Union[discord.Member, discord.User],timeout: int = 30):
+	def __init__(self, user: Union[discord.Member, discord.User],timeout: int = 30, message:discord.Message = None):
 		super().__init__(timeout=timeout)
 		self.value = None
 		self.user = user
+		self.message = message
+		self.interaction: discord.Interaction = None
 
 	async def on_timeout(self):		
-		self.value = None
+		for button in self.children:
+			button.disabled = True
+		await self.message.edit(view=self)
+		self.stop()
 	
 	async def interaction_check(self, interaction: discord.Interaction) -> bool:
 		if interaction.user.id == self.user.id:
@@ -20,10 +25,12 @@ class Confirm(discord.ui.View):
 
 	@discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
 	async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+		self.interaction = interaction
 		self.value = True
 		self.stop()
 
 	@discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
 	async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+		self.interaction = interaction
 		self.value = False
 		self.stop()
