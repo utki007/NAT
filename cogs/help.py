4758947a1dcb02513b965a8a96665395477e0508
Jay Patel <pateljay24004@gmail.com>
2022-09-11 13:48:41 +0530

    updated help for sub-comamnds groups

--- cogs/help.py
+++ cogs/help.py
@@ -24,7 +24,7 @@ class Help(commands.Cog):
         embed = discord.Embed(title=f"Help | {command.name.title()}", description=f"{command.description}", color=interaction.client.color['default'])
         useage = ""
         query = ""
-        useage += f"/{command.name}"
+        useage += f"/{command.name} "
         if type(command) == app_commands.commands.Command:
             if len(command.parameters) > 0:
                 extra_flags = False
@@ -35,11 +35,11 @@ class Help(commands.Cog):
                     else:
                         extra_flags = True
 
-                    query += f"**{argument.display_name}:** `{argument.description}`\n"
+                    query += f"**{argument.display_name.capitalize()}:** `{argument.description}`\n"
                 if extra_flags == True:
                     useage += f" <Extra Flags>"
-                embed.add_field(name="Usage:", value=f"`{useage}`")
-                embed.add_field(name="Options:", value=f"{query}", inline=False)
+                embed.add_field(name="Usage:", value=f"\n`{useage}`")
+                embed.add_field(name="Options:", value=f"\n{query}", inline=False)
                 embed.set_footer(text=f"Note: arguments in [] are required, arguments in <> are optional")
             
             if command.extras != None:
@@ -49,6 +49,7 @@ class Help(commands.Cog):
                     pass
             await interaction.response.send_message(embed=embed)
         elif type(command) == app_commands.commands.Group:
+
             embed = discord.Embed(title=f"Help | {command.name}", description=f"{command.description}", color=interaction.client.color['default'])
             embed.description += f"\n\n**Available Commands:**\n"
             for subcommand in command.commands:
@@ -56,26 +57,52 @@ class Help(commands.Cog):
             embed.set_footer(text=f"Use buttons to know more about each command")
             pages = [embed]
             for sub_command in command.commands:
-                sub_cmd_embed = discord.Embed(title=f"{command.name.title()} {sub_command.name}", description=f"{sub_command.description}", color=interaction.client.color['default'])
-                useage = ""
-                query = ""
-                useage += f"/{command.name} {sub_command.name}"
-                if len(sub_command.parameters) > 0:
-                    extra_flags = False
-                    for argument in sub_command.parameters:
-                        if argument.required:
-                            useage += f" [{argument.display_name}]"
-                            
-                        else:
-                            extra_flags = True
+                if type(sub_command) == app_commands.commands.Command:
+                    sub_cmd_embed = discord.Embed(title=f"{command.name.title()} {sub_command.name}", description=f"{sub_command.description}", color=interaction.client.color['default'])
+                    useage = ""
+                    query = ""
+                    useage += f"/{command.name} {sub_command.name}"
+                    if len(sub_command.parameters) > 0:
+                        extra_flags = False
+                        for argument in sub_command.parameters:
+                            if argument.required:
+                                useage += f" [{argument.display_name}]"
+                                
+                            else:
+                                extra_flags = True
 
-                        query += f"**{argument.display_name}:** `{argument.description}`\n"
-                    if extra_flags == True:
-                        useage += f" <Extra Flags>"
-                    sub_cmd_embed.add_field(name="Usage:", value=f"`{useage}`")
-                    sub_cmd_embed.add_field(name="Options:", value=f"{query}", inline=False)
-                    sub_cmd_embed.set_footer(text=f"Note: arguments in [] are required, arguments in <> are optional")
-                pages.append(sub_cmd_embed)
+                            query += f"**{argument.display_name.capitalize()}:** `{argument.description}`\n"
+                        if extra_flags == True:
+                            useage += f" <Extra Flags>"
+                        sub_cmd_embed.add_field(name="Usage:", value=f"\n`{useage}`")
+                        sub_cmd_embed.add_field(name="Options:", value=f"\n{query}", inline=False)
+                        sub_cmd_embed.set_footer(text=f"Note: arguments in [] are required, arguments in <> are optional")
+                    pages.append(sub_cmd_embed)
+
+                elif type(sub_command) == app_commands.commands.Group:
+                    for sub_cmd_groub in sub_command.commands:
+                        if type(sub_cmd_groub) == app_commands.commands.Command:
+                            sub_cmd_embed = discord.Embed(title=f"{command.name.title()} {sub_command.name} {sub_cmd_groub.name}", description=f"{sub_cmd_groub.description}", color=interaction.client.color['default'])
+                            useage = ""
+                            query = ""
+                            useage += f"/{command.name} {sub_command.name} {sub_cmd_groub.name}"
+                            if len(sub_cmd_groub.parameters) > 0:
+                                extra_flags = False
+                                for argument in sub_cmd_groub.parameters:
+                                    if argument.required:
+                                        useage += f" [{argument.display_name}]"
+                                        
+                                    else:
+                                        extra_flags = True
+
+                                    query += f"**{argument.display_name.capitalize()}:** `{argument.description}`\n"
+                                if extra_flags == True:
+                                    useage += f" <Extra Flags>"
+                                sub_cmd_embed.add_field(name="Usage:", value=f"\n`{useage}`")
+                                sub_cmd_embed.add_field(name="Options:", value=f"\n{query}", inline=False)
+                                sub_cmd_embed.set_footer(text=f"Note: arguments in [] are required, arguments in <> are optional")
+                            pages.append(sub_cmd_embed)
+
             custom_button = [discord.ui.Button(label="Previous", style=discord.ButtonStyle.blurple),discord.ui.Button(label="Stop", style=discord.ButtonStyle.red),discord.ui.Button(label="Next", style=discord.ButtonStyle.blurple)]
 
             await Paginator(interaction, pages, custom_button).start(embeded=True, quick_navigation=False)
