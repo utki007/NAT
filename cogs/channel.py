ea8e260bfca6d8fa57d3167fcd20c7508ea4d0ef
Utkarsh Narain <utkarshnarain007@gmail.com>
2023-01-14 11:32:40 +0530

    Added slowmode

--- cogs/channel.py
+++ cogs/channel.py
@@ -3,6 +3,7 @@ import discord
 from discord import app_commands, Interaction
 from discord.ext import commands
 from utils.convertor import *
+from utils.transformers import TimeConverter
 from typing import Union, List
 from ui.channel import *
 
@@ -23,6 +24,59 @@ class channel(commands.GroupCog, name="channel"):
 		]
 		return choice
 	
+	@app_commands.command(name="slowmode", description="Set cooldown for chat ⏰")
+	@app_commands.describe(time="Enter time. Ex: '1h5m4s'")
+	async def slowmode(self, interaction: discord.Interaction, time: str="0s"):
+		if interaction.user.id == 685705841264820247:
+			await interaction.response.defer(ephemeral = True)
+			await interaction.edit_original_response(
+				content="slowmode will be set shortly..."
+			)
+		else:
+			await interaction.response.defer(ephemeral = False)
+		
+		channel = interaction.channel
+
+		try:
+			time = await convert_to_time(time)
+			cd = int(await calculate(time))
+		except:
+			warning = discord.Embed(
+				color=0xDA2A2A,
+				description=f"<a:nat_cross:1010969491347357717> **|** Incorrect time format, please use `h|m|s`")
+			return await interaction.edit_original_response(embed=warning)
+
+		if cd > 21600:
+			warning = discord.Embed(
+				color=0xDA2A2A,
+				description=f"<a:nat_cross:1010969491347357717> **|** Slowmode interval can't be greater than 6 hours.")
+			return await interaction.edit_original_response(embed=warning)
+		elif cd == 0:
+			await channel.edit(slowmode_delay=cd)
+			await interaction.edit_original_response(content=f"Slowmode has been removed!! 🎉")
+		else:
+			await channel.edit(slowmode_delay=cd)
+			timer = datetime.datetime.strptime(str(datetime.timedelta(seconds=cd)), '%H:%M:%S')
+			cd = int(cd)
+			desc = f''
+			if timer.hour>0:
+				if timer.hour == 1:
+					desc = desc + f'{timer.hour} hour '
+				else:
+					desc = desc + f'{timer.hour} hours '
+			if timer.minute>0:
+				if timer.minute == 1:
+					desc = desc + f'{timer.minute} minute '
+				else:
+					desc = desc + f'{timer.minute} minutes '
+			if timer.second>0:
+				if timer.second == 1:
+					desc = desc + f'{timer.second} second '
+				else:
+					desc = desc + f'{timer.second} seconds '
+
+			await interaction.edit_original_response(content=f'Slowmode for {channel.mention} has been set to **{desc}**.')
+
 	@app_commands.command(name="lock", description="Lock channel 🙊", extras={'example': '/lock'})
 	@app_commands.describe(role = "Provide role", user = "Input user 👤")
 	async def lock(self, interaction:  discord.Interaction, role: discord.Role = None, user: discord.User = None):
