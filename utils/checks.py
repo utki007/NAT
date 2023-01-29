295fd6b1b26fa41dca7e59b33bf237a2cbe08284
Utkarsh Narain <utkarshnarain007@gmail.com>
2023-01-29 23:56:10 +0530

    Updated checks to include server owners!

--- utils/checks.py
+++ utils/checks.py
@@ -9,22 +9,14 @@ class App_commands_Checks():
             if Interaction.user.id in Interaction.client.owner_ids:
                 return True
             else:
-                raise Unauthorized(Interaction)
+                return False
         return app_commands.check(predicate)
-
-    def is_bot_mod():
+    
+    def is_server_owner():
         async def predicate(Interaction: Interaction) -> bool:
-            if Interaction.user.id in Interaction.client.bot_mod_ids:
+            if Interaction.user.id == Interaction.guild.owner.id:
                 return True
             else:
-                raise Unauthorized(Interaction)
+                return False
         return app_commands.check(predicate)
-
-
-#custom Errors for custom checks
-            
-class Unauthorized(app_commands.AppCommandError):
-    def __init__(self, user, *args, **kwargs):
-        self.user = user
-        super().__init__(*args, **kwargs)
         
