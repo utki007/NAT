d589aada0110f8030b62fd85d2481d14a080e341
Utkarsh Narain <utkarshnarain007@gmail.com>
2023-02-02 22:03:23 +0530

    changed, member.add_roles to member.edit

--- utils/functions.py
+++ utils/functions.py
@@ -1,5 +1,7 @@
 import discord
 
+from utils.embeds import get_warning_embed
+
 def clean_code(content):
     if content.startswith("```") and content.endswith("```"):
         return "\n".join(content.split("\n")[1:])[:-3]
@@ -12,25 +14,24 @@ async def quarantineUser(bot, user: discord.Member, quarantineRole: discord.Role
         if role == user.guild.default_role:
             continue
         roles.append(role.id)
-    roles_to_Remove = [role for role in user.roles if role != user.guild.default_role]
-    await user.remove_roles(*roles_to_Remove)
-    await user.add_roles(quarantineRole)
+    try:
+        await user.edit(roles=[quarantineRole])
+    except:
+        embed = await get_warning_embed(f"Unable to quarantine {user.mention} in {user.guild.name}.")
+        return False
     data = await bot.quarantinedUsers.find(user.guild.id)
     if not data:
         data = {"_id": user.guild.id, "users": {}}
     data['users'][str(user.id)] = roles
     await bot.quarantinedUsers.upsert(data)
+    return True
 
 async def unquarantineUser(bot, user: discord.Member):
     data = await bot.quarantinedUsers.find(user.guild.id)
-    roles_to_Remove = [role for role in user.roles if role != user.guild.default_role]
-    await user.remove_roles(*roles_to_Remove)
     if data:
         if str(user.id) in data['users']:
-            roles = []
-            for role in data['users'][str(user.id)]:
-                roles.append(user.guild.get_role(role))
-            await user.add_roles(*roles)
+            roles = [user.guild.get_role(role_id) for role_id in data['users'][str(user.id)]]
+            await user.edit(roles=roles)
             del data['users'][str(user.id)]
             await bot.quarantinedUsers.upsert(data)
             return True
