ad1e0a299b57ff18fdf7813bd875a15539acd79e
Utkarsh Narain <utkarshnarain007@gmail.com>
2023-03-29 23:58:10 +0530

    Updated bot status

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ class MyBot(commands.Bot):
 		members_list = [len(guild.members) for guild in  bot.guilds]
 		total_members = sum(members_list)
 
-		await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.listening, name=f"{total_members} members!"))
+		await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=f"Beta Version 2.0.2!"))
 
 bot = MyBot()
 
