96897210df0eea25ab5fa1d8eef568fa14bc15e0
Utkarsh Narain <utkarshnarain007@gmail.com>
2023-03-29 23:13:39 +0530

    added support for bot owner

--- utils/checks.py
+++ utils/checks.py
@@ -14,7 +14,7 @@ class App_commands_Checks():
     
     def is_server_owner():
         async def predicate(Interaction: Interaction) -> bool:
-            if Interaction.user.id == Interaction.guild.owner.id:
+            if Interaction.user.id == Interaction.guild.owner.id or Interaction.user.id in Interaction.client.owner_ids:
                 return True
             else:
                 return False
