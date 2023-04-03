13c97c943665f82035d9714e6d575afe6ee33f62
Utkarsh Narain <utkarshnarain007@gmail.com>
2023-04-03 12:22:12 +0530

    Updated quarantine logic

--- utils/functions.py
+++ utils/functions.py
@@ -17,18 +17,21 @@ async def quarantineUser(bot, user: discord.Member, quarantineRole: discord.Role
     roles = [role for role in user.roles if role != user.guild.default_role]
     roles_to_keep = [role for role in roles if role.position >= top_role_position or role.managed]
     roles_to_remove = [role for role in roles if role.position < top_role_position and not role.managed]
-    roles_to_remove.reverse()
     roles_to_remove_id = [role.id for role in roles_to_remove if role != quarantineRole]
 
-    if quarantineRole is not None and quarantineRole not in user.roles and quarantineRole.position < top_role_position:
+    if quarantineRole is not None and quarantineRole.position < top_role_position:
         roles_to_keep.append(quarantineRole)
-    
     await user.edit(roles = roles_to_keep, reason=reason)
 
     data = await bot.quarantinedUsers.find(user.guild.id)
     if not data:
         data = {"_id": user.guild.id, "users": {}}
-    data['users'][str(user.id)] = roles_to_remove_id
+        data['users'][str(user.id)] = roles_to_remove_id
+    else:
+        if str(user.id) in data['users'] and len(roles_to_remove_id) > 0:
+            data['users'][str(user.id)].extend(roles_to_remove_id)
+        else:
+            data['users'][str(user.id)] = roles_to_remove_id
     await bot.quarantinedUsers.upsert(data)
 
 async def unquarantineUser(bot, user: discord.Member, quarantineRole: discord.Role = None, reason: str = None):
@@ -46,6 +49,4 @@ async def unquarantineUser(bot, user: discord.Member, quarantineRole: discord.Ro
             await user.edit(roles=set(roles_to_add), reason=reason)
             del data['users'][str(user.id)]
             await bot.quarantinedUsers.upsert(data)
-    else:
-        if quarantineRole is not None and quarantineRole in user.roles and quarantineRole.position < user.guild.me.top_role.position:
-            await user.remove_roles(quarantineRole, reason=reason)
+            return True
