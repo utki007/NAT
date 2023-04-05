e671682cce8e7dd314889c0727e7e7f37bbe4773
Utkarsh Narain <utkarshnarain007@gmail.com>
2023-04-05 23:20:27 +0530

    Updated pool safety logic

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 import logging.handlers
 import os
 from ast import literal_eval
-
+import re
 import discord
 import motor.motor_asyncio
 from discord.ext import commands
@@ -88,13 +88,17 @@ async def on_message_edit(before, after):
 		# for serversettings in dank
 		if message.interaction is not None and message.interaction.name == 'serversettings':
 			if message.embeds[0].to_dict()['title'] == 'Events Manager':
-				managerRole = int(message.embeds[0].to_dict()['description'].split(" ")[-1])
-				data = await bot.dankSecurity.find(message.guild.id)
-				if not data:
-					data = {"_id": message.guild.id, "event_manager": None, "whitelist": [], "quarantine": None}
-				if data['event_manager'] != managerRole:
-					data['event_manager'] = managerRole
-					await bot.dankSecurity.upsert(data)
+				managerRole = None
+				description = message.embeds[0].to_dict()['description']
+				idList = re.findall("(\d{18,19})", description)
+				if len(idList) > 0:
+					managerRole = int(idList[0])
+					data = await bot.dankSecurity.find(message.guild.id)
+					if not data:
+						data = {"_id": message.guild.id, "event_manager": None, "whitelist": [], "quarantine": None}
+					if data['event_manager'] != managerRole:
+						data['event_manager'] = managerRole
+						await bot.dankSecurity.upsert(data)
 
 	# return if message is from bot
 	if message.author.bot:
@@ -104,29 +108,28 @@ async def on_message_edit(before, after):
 async def on_member_update(before, after):
 	member = after
 
-	after_roles = set([role.id for role in after.roles])
-	before_roles = set([role.id for role in before.roles])
-	roles = list(after_roles - before_roles)
-
-	if len(roles) > 0:
-		data = await bot.dankSecurity.find(member.guild.id)
-		if data:
-			if data['event_manager'] in roles and member.id not in data['whitelist']:
-				securityLog = bot.get_channel(1089973828215644241)
+	roles = [role.id for role in after.roles]
+	
+	data = await bot.dankSecurity.find(member.guild.id)
+	if data:
+		event_manager = member.guild.get_role(data['event_manager'])
+		if event_manager is not None and event_manager in member.roles and member.id not in data['whitelist']: 
+			securityLog = bot.get_channel(1089973828215644241)
+			if securityLog is not None:
 				await securityLog.send(f"{member.mention} has made an unauthorized attempt to get **Dank Manager role** in {member.guild.name}.")
-				try:
-					await member.remove_roles(member.guild.get_role(data['event_manager']), reason="Member is not a authorized Dank Manager.")
-				except:
-					pass
-				role = None
-				if data['quarantine'] is not None:					
-					role = member.guild.get_role(data['quarantine'])
-				await quarantineUser(bot, member, role, f"{member.name}#{member.discriminator} (ID: {member.id}) has made an unauthorized attempt to get Dank Manager role.")					
-				try:
-					embed = await get_warning_embed(f"{member.mention} has made an unsucessful attempt to get Dank Manager role in {member.guild.name}")
-					await member.guild.owner.send(embed = embed)
-				except:
-					pass
+			try:
+				await member.remove_roles(member.guild.get_role(data['event_manager']), reason="Member is not a authorized Dank Manager.")
+			except:
+				pass
+			role = None
+			if data['quarantine'] is not None:					
+				role = member.guild.get_role(data['quarantine'])
+			await quarantineUser(bot, member, role, f"{member.name}#{member.discriminator} (ID: {member.id}) has made an unauthorized attempt to get Dank Manager role.")					
+			try:
+				embed = await get_warning_embed(f"{member.mention} has made an unsucessful attempt to get Dank Manager role in {member.guild.name}")
+				await member.guild.owner.send(embed = embed)
+			except:
+				pass
 
 # @bot.event
 # async def on_guild_join(guild):
