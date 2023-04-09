e6856b023d956fad7c2f1662b6bc09d1d31505f7
Utkarsh Narain <utkarshnarain007@gmail.com>
2023-04-10 00:23:47 +0530

    Update example.py

--- cogs/example.py
+++ cogs/example.py
@@ -176,8 +176,8 @@ class donation(commands.Cog):
 			1: {'icon': (58, 265), 'name': (176, 273), 'donated': (176, 309)},
 			2: {'icon': (58, 380), 'name': (176, 392), 'donated': (176, 428)}}
 
-		draw.text((116, 28), f"{guild.name}", font=font, fill="#DADBE3") #guild name 
-		draw.text((116, 61), f"{event_name}", font=winner_name_font, fill="#DADBE3") #event name
+		draw.text((116, 28), f"{guild.name}", font=winner_name_font, fill="#9A9BD5") #guild name 
+		draw.text((116, 61), f"{event_name}", font=winner_name_font, fill="#9A9BD5") #event name
 
 		for i in data[:3]:
 			user = i['user']
